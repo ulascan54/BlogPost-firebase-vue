29e16255d5bf1a8c04abb39229c400029cd6d766
ulascan54 <ulascandemirbag@gmail.com>
2021-10-05 09:44:42 +0300

    blog page complate

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="blog-card">
-    <div class="icons">
+    <div v-show="editPost" class="icons">
       <div class="icon">
         <Edit class="edit" />
       </div>
@@ -33,6 +33,11 @@ export default {
     Edit,
     Delete,
   },
+  computed:{
+    editPost(){
+      return this.$store.state.editPost
+    }
+  }
 };
 </script>
 <style lang="scss">
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="left">
         <div class="col-1">
-          <router-link class="header" :to="{ name: 'home' }">BlogPosts</router-link
+          <router-link class="header" :to="{ name: 'Home' }">BlogPosts</router-link
           >
           <ul>
             <li>
@@ -22,14 +22,14 @@
         </div>
         <div class="col-2">
           <ul>
-            <router-link class="link" :to="{ name: 'home' }">Home</router-link>
-            <router-link class="link" :to="{ name: 'blogs' }"
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="link" :to="{ name: 'Blogs' }"
               >Blogs</router-link
             >
             <router-link v-if="user" class="link" :to="{ name: 'newposts' }"
               >Create Post</router-link
             >
-            <router-link v-if="!user" class="link" :to="{ name: 'home' }"
+            <router-link v-if="!user" class="link" :to="{ name: 'Home' }"
               >Login / Register</router-link
             >
           </ul>
