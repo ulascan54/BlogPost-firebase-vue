21b50736f11c6c0c7fcea19841a65802285a217e
ulascan54 <ulascandemirbag@gmail.com>
2021-10-10 23:56:59 +0300

    get user info from firebase and it save in state

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,70 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../boot/firebaseInit";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    sampleBlogCards:[
-      {blogTitle:"Blog Card #1", blogCoverPhoto:"stock-1", blogDate:"May 1, 2021"},
-      {blogTitle:"Blog Card #2", blogCoverPhoto:"stock-2", blogDate:"May 1, 2021"},
-      {blogTitle:"Blog Card #3", blogCoverPhoto:"stock-3", blogDate:"May 1, 2021"},
-      {blogTitle:"Blog Card #4", blogCoverPhoto:"stock-4", blogDate:"May 1, 2021"}
+    sampleBlogCards: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "stock-2",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "stock-3",
+        blogDate: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "stock-4",
+        blogDate: "May 1, 2021",
+      },
     ],
-    editPost:null
+    editPost: null,
+    user: null,
+    profileEmail: null,
+    profileFristName: null,
+    profileLastName: null,
+    profileUserName: null,
+    profileId: null,
+    profileInitials: null,
   },
   mutations: {
-    toggleEditPost(state,payload){
-      state.editPost=payload;
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
       console.log(state.editPost);
-    }
+    },
+    setProfileInfo(state, doc) {
+      (state.profileId = doc.id), (state.profileEmail = doc.data().email);
+      state.profileFristName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUserName = doc.data().userName;
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFristName.match(/(\b\S?/g).join("") +
+        state.profileLastName.match(/(\b\S?/g).join("");
+    },
   },
+
   actions: {
+    async getCurrentUser({ commit }) {
+      const dataBase = await db
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid);
+      const dbResult = await dataBase.get();
+      commit("setProfileInfo", dbResult);
+      commit("setProfileInitals");
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
