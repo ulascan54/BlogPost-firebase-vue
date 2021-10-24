bbd4af79415b93de05b6f9219cc8af8379cfcc13
ulascan54 <ulascandemirbag@gmail.com>
2021-10-24 23:44:05 +0300

    footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,8 +24,8 @@
           <ul>
             <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
             <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
-            <router-link v-if="user" class="link" to="#">Create Post</router-link>
-            <router-link v-if="!user" class="link" :to="{ name: 'Login' }">Login / Register</router-link>
+            <router-link v-if="user" class="link" to="#">Gönderi Oluştur</router-link>
+            <router-link v-if="!user" class="link" :to="{ name: 'Login' }">Giriş / Kayıt Ol</router-link>
           </ul>
         </div>
       </div>
